fd9eb80a886598ebb58099fe34c0c27c6415e008
Harcic Yang <harcic@outlook.com>
2026-08-20 15:49:43 +0800

    fix: temporarily disable the patch

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 import asyncio
 
-from euleronebot import Adapter, LagrangeProtocol, apply_patches, setup
+from euleronebot import Adapter, LagrangeProtocol, setup
 
 logger, cfg = setup()
 
 if __name__ == "__main__":
     logger.info("Euler OneBot")
-    apply_patches()
+    # apply_patches()
     adapter = Adapter(impls=cfg.connections, access_token=cfg.access_token)
     protocol = LagrangeProtocol(cfg, adapter)
     asyncio.run(protocol.run())
